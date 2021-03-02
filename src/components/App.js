44700a951509daa6ad505788556981a1fe23d956
george <georit01@icloud.com>
2021-03-02 12:24:14 -0600

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Square from './Square';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Here are all the squares...</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/Square.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Square.css';
+
+export default function Square(props) {
+  return <div className="Square">I am a Square!</div>;
+}
